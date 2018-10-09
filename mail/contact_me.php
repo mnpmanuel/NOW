392240c6362019a6a797383cecd0e4b6761146ed
Manuel Palmeira <mnp.manuel@gmail.com>
2018-10-09 23:07:51 +0100

    Contact form finaly working but just for Porto and without Dropdown info

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,16 +1,34 @@
 <?php
-// Check for empty fields
-if(empty($_POST['local'])     ||
-   empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['space'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
+
+#
+# Exemplo de envio de e-mail SMTP PHPMailer
+#
+# Inclui os ficheiros class.phpmailer.php localizado na pasta phpmailer
+require_once("phpmailer/class.phpmailer.php");
+require_once("phpmailer/class.smtp.php");
+
+# Inicia a classe PHPMailer
+$mail = new PHPMailer();
+
+# Define os dados do servidor e tipo de conexão
+$mail->IsSMTP(); // Define que a mensagem será SMTP
+$mail->Host = "localhost"; # Endereço do servidor SMTP, na WebHS basta usar localhost caso a conta de email esteja na mesma máquina de onde esta a correr este código, caso contrário altere para o seu desejado ex: mail.nomedoseudominio.pt
+$mail->Port = 587; // Porta TCP para a conexão
+$mail->SMTPAutoTLS = false; // Utiliza TLS Automaticamente se disponível
+$mail->SMTPAuth = true; # Usar autenticação SMTP - Sim
+$mail->Username = 'noreply@no-office-work.com'; # Login de e-mail
+$mail->Password = 'Penacova2019'; // # Password do e-mail
+# Define o remetente (você)
+$mail->From = "noreply@no-office-work.com"; # Seu e-mail
+$mail->FromName = "No Office Work"; // Seu nome
+# Define os destinatário(s)
+$mail->AddAddress('mp@imatch.pt', 'Pessoa Nome 1'); # Os campos podem ser substituidos por variáveis
+#$mail->AddAddress('webmaster@nomedoseudominio.pt'); # Caso queira receber uma copia
+#$mail->AddCC('pessoa2@dominio.pt', 'Pessoa Nome 2'); # Copia
+#$mail->AddBCC('pessoa3@dominio.pt', 'Pessoa Nome 3'); # Cópia Oculta
+# Define os dados técnicos da Mensagem
+$mail->IsHTML(true); # Define que o e-mail será enviado como HTML
+#$mail->CharSet = 'iso-8859-1'; # Charset da mensagem (opcional)
 
 $local = strip_tags(htmlspecialchars($_POST['local']));
 $name = strip_tags(htmlspecialchars($_POST['name']));
@@ -19,12 +37,25 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $space = strip_tags(htmlspecialchars($_POST['space']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-// Create the email and send the message
-$to = 'mp@imatch.pt'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "NOW Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nLocation: $local\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nWants: $space\n\nMessage:\n$message";
-$headers = "From: noreply@no-office-work.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
-return true;
+# Define a mensagem (Texto e Assunto)
+$mail->Subject = "NOW Contact Form:  $name"; # Assunto da mensagem
+$mail->Body = "You have received a new message from your website contact form.<br>"."Here are the details:<br>Location: $local<br>Name: $name<br>Email: $email_address<br>Phone: $phone<br>Wants: $space<br>Message: $message";
+$mail->AltBody = "Este é o corpo da mensagem de teste, somente Texto! \r\n :)";
+
+# Define os anexos (opcional)
+#$mail->AddAttachment("c:/temp/documento.pdf", "documento.pdf"); # Insere um anexo
+# Envia o e-mail
+$enviado = $mail->Send();
+
+# Limpa os destinatários e os anexos
+$mail->ClearAllRecipients();
+$mail->ClearAttachments();
+
+# Exibe uma mensagem de resultado (opcional)
+// if ($enviado) {
+// echo "E-mail enviado com sucesso!";
+// } else {
+// echo "Não foi possível enviar o e-mail.";
+// echo "<b>Informações do erro:</b> " . $mail->ErrorInfo;
+// }
 ?>
